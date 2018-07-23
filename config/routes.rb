bd691bc414a56ae20908aea25648ecdd07870f2d
Naveed Shah <naveed@tayybah.com>
2018-07-23 10:06:14 -0400

    first commit to github

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root :to =>'application#index'
+
+  devise_scope :user do
+    get '/logout',  :to => 'sessions#destroy'
+  end
+
 end
