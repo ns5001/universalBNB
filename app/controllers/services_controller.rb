3511d91e4fea620e989c6f61efd2571f3352ae43
Naveed Shah <naveed@tayybah.com>
2018-07-27 19:33:39 -0400

    working on the index page

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,7 +1,7 @@
 class ServicesController < ApplicationController
 
   def show
-    # I want to be able to pass in the service object
+    binding.pry
     @service = Service.find_by_id(params[:id])
   end
 
