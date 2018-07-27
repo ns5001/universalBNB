0e12b827498de336ffdc1f62297e13ec6b02a8ef
Glyne J. Gittens <Jgit121@gmail.com>
2018-07-27 18:08:17 -0400

    Deleted Gemfile.lock to prevent merge errors. Also added it to gitignore. Fixed a migration typo. Ran a migration that changed the schema.rb

--- db/migrate/20180722012507_create_services.rb
+++ db/migrate/20180722012507_create_services.rb
@@ -1,7 +1,7 @@
 class CreateServices < ActiveRecord::Migration[5.0]
   def change
     create_table :services do |t|
-      t.string :type          null: false, default: ""
+      t.string :type,    null: false, default: ""
       t.timestamps
     end
   end
